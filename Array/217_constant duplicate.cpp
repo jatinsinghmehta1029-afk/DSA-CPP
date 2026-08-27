643141f35a6a62a3eec2e1c4e29998f1b46e5b8a
jatin singh mehta <jatinsinghmehta1029@gmail.com>
2026-08-27 22:23:30 +0530

    i used hash map but could be done using sorting and checking prinious eliment but have to learn new topics

---
+++ Array/217_constant duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int num:nums){
+            mp[num]++;
+        
+        if(mp[num]>=2){
+            return true;
+        }
+        else false;
+        }return 0;
+    }
+};
